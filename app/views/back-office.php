9429d61fbab7353e2feff17525e1f8b9eaeafadd
Spottingtrains <florian.beugnies@apprenant.ifapme.be>
2026-05-11 20:24:24 +0200

    TODO comment for role assignment in back-office

--- app/views/back-office.php
+++ app/views/back-office.php
@@ -1,8 +1,8 @@
 <?php
 require __DIR__ . '/nav/header.php';
 require_once __DIR__ . '/../controllers/authController.php';
-// TODO: DEV ONLY - à supprimer avant livraison
-//  donne le role admin pour tester l'accès au back-office 
+
+// TODO: DEV ONLY - à supprimer avant livraison - donne le role admin pour tester l'accès au back-office 
 checkRole(3);
 ?>
 
