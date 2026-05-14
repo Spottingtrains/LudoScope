2c80abb85d5786f45bfd4b4b98d0c4657efaba2b
Spottingtrains <florian.beugnies@apprenant.ifapme.be>
2026-05-14 03:53:01 +0200

    fix(home): update TODO comment to clarify Bootstrap class verification

--- app/views/home.php
+++ app/views/home.php
@@ -15,7 +15,7 @@ require_once __DIR__ . '/../../app/middleware/auth.php';
         <?php unset($_SESSION['success']); ?>
     <?php endif; ?>
     <!-- Section hero et CTA -->
-    <!-- TODO: vérifier les classes Bootstrap -->
+    <!-- TODO: vérifier les classes Bootstrap et l'intérêt des div -->
     <section class="py-24 ">
         <div class="container mw-screen-xl">
             <div class="bg-body rounded shadow-soft-3 border p-5">
